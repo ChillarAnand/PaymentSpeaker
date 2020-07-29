d1d123210de2373724994208622f370cfb0f1701
Chillar Anand <anand21nanda@gmail.com>
2020-07-29 19:03:01 +0530

    Announce only financial messages on speaker

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ An android app to announce the payments received by reading incoming SMS.
 Consider a juice shop owner who setup a UPI(PayTM, GPay, PhonePhe, BHIM etc) merchant account. Now whenever a customer makes a payment, he has to stop his work and check his phone to verify the completed payment.
 
 Instead of that, this app will announce the payment on speaker as soon as payment is made. This improves productivity of the merchants by saving their time.
+
+
+### Credits
+
+Modified Icon by <a href="https://pixabay.com/users/95C-484762/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1885354">95C</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1885354">Pixabay</a>
--- app/src/main/java/com/avilpage/paymentspeaker/MessageReceiver.java
+++ app/src/main/java/com/avilpage/paymentspeaker/MessageReceiver.java
@@ -6,10 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 
+
 public class MessageReceiver extends BroadcastReceiver {
 
     private static MessageListener mListener;
-//    private Speaker speaker = new Speaker();
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -19,26 +19,19 @@ public class MessageReceiver extends BroadcastReceiver {
         for(int i=0; i<pdus.length; i++){
             SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) pdus[i]);
             String messageBody = smsMessage.getMessageBody();
-            String message = "Sender : " + smsMessage.getDisplayOriginatingAddress()
-                    + "Email From: " + smsMessage.getEmailFrom()
-                    + "Emal Body: " + smsMessage.getEmailBody()
-                    + "Display message body: " + smsMessage.getDisplayMessageBody()
-                    + "Time in millisecond: " + smsMessage.getTimestampMillis()
-                    + "Message: " + messageBody;
+//            String message = "Sender : " + smsMessage.getDisplayOriginatingAddress()
+//                    + "Email From: " + smsMessage.getEmailFrom()
+//                    + "Emal Body: " + smsMessage.getEmailBody()
+//                    + "Display message body: " + smsMessage.getDisplayMessageBody()
+//                    + "Time in millisecond: " + smsMessage.getTimestampMillis()
+//                    + "Message: " + messageBody;
             if (messageBody != null) {
-                readMessage(messageBody);
-//                speaker.speak(messageBody);
+                mListener.messageReceived(messageBody);
             }
-            mListener.messageReceived(messageBody);
         }
     }
 
-    public void readMessage(String messageBody) {
-
-    }
-
     public static void bindListener(MessageListener listener){
-        System.out.printf("-----------------MR");
         mListener = listener;
     }
 }
