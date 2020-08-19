9f5e73a6d54aa589682f2ec2d6e0b03811d2b8b6
Chillar Anand <anand21nanda@gmail.com>
2020-08-19 20:46:44 +0530

    Cleanup code

--- app/src/main/java/com/avilpage/paymentspeaker/MainActivity.java
+++ app/src/main/java/com/avilpage/paymentspeaker/MainActivity.java
@@ -5,27 +5,18 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.speech.tts.TextToSpeech;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
-import java.util.Locale;
 import java.util.Random;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public class MainActivity extends AppCompatActivity implements MessageListener {
-    private TextToSpeech textToSpeech;
+
+public class MainActivity extends AppCompatActivity {
     private Button testButton;
-    private String parsedMessage;
-    private String amount;
-    private String amountInRupees;
-    private static final Pattern amountPattern = Pattern.compile("\\d+\\.\\d{2} ");
     private Random random = new Random();
     private int randomNumber;
 
@@ -34,10 +25,11 @@ public class MainActivity extends AppCompatActivity implements MessageListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         checkPermissions();
-        initTTS();
         setListeners();
-        MessageReceiver.bindListener(this);
-        startService(new Intent(this, SpeakerService.class));
+        Intent backgroundService = new Intent(getApplicationContext(), SpeakerService.class);
+        startService(backgroundService);
+        Intent backgroundService2 = new Intent(getApplicationContext(), MessageReceiver.class);
+        startService(backgroundService2);
     }
 
     private void setListeners() {
@@ -47,45 +39,14 @@ public class MainActivity extends AppCompatActivity implements MessageListener {
             @Override
             public void onClick(View arg0) {
                 randomNumber = random.nextInt(1000);
-                String data = String.format("Credited. %s Rupees.", randomNumber);
-                Log.i("TTS", "button clicked: " + data);
-                speak(data);
+                String message = String.format("Credited. %s Rupees.", randomNumber);
+                Context context = getApplicationContext();
+                Intent serviceIntent = new Intent(context, SpeakerService.class);
+                serviceIntent.putExtra(SpeakerService.MESSAGE, message);
+                context.startService(serviceIntent);
             }
 
         });
-    }
-
-    public void speak(String speech) {
-        if (textToSpeech == null) {
-            initTTS();
-        }
-        int speechStatus = textToSpeech.speak(speech, TextToSpeech.QUEUE_FLUSH, null);
-
-        if (speechStatus == TextToSpeech.ERROR) {
-            Log.e("TTS", "Error in converting Text to Speech!");
-        }
-    }
-
-    private void initTTS() {
-        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
-            @Override
-            public void onInit(int status) {
-                if (status == TextToSpeech.SUCCESS) {
-                    int ttsLang = textToSpeech.setLanguage(Locale.US);
-
-                    if (ttsLang == TextToSpeech.LANG_MISSING_DATA
-                            || ttsLang == TextToSpeech.LANG_NOT_SUPPORTED) {
-                        Log.e("TTS", "The Language is not supported!");
-                    } else {
-                        Log.i("TTS", "Language Supported.");
-                    }
-                    Log.i("TTS", "Initialization success.");
-                } else {
-                    Toast.makeText(getApplicationContext(), "TTS Initialization failed!", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-        textToSpeech.setSpeechRate((float) 0.9);
     }
 
     private void checkPermissions() {
@@ -98,34 +59,4 @@ public class MainActivity extends AppCompatActivity implements MessageListener {
         }
     }
 
-    @Override
-    public void messageReceived(String message) {
-        parsedMessage = parse(message);
-        if (parsedMessage == null) {
-            return;
-        }
-        speak(parsedMessage);
-    }
-
-    private String parse(String message) {
-        message = message.toLowerCase();
-        message = message.replaceAll(",", "");
-        if (!message.contains("paid") && !message.contains("credited")) {
-            return null;
-        }
-        Matcher matcher = amountPattern.matcher(message);
-        if (!matcher.find()) {
-            return null;
-        }
-        amount = matcher.group(0);
-        amountInRupees = amount.split("\\.")[0];
-        parsedMessage = "Credited. " + amountInRupees + "rupees.";
-        return parsedMessage;
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-//        textToSpeech.shutdown();
-    }
 }
